eccc00dd008bd74e6eb5efe7bdb3c8cab284d047
Mayank Joshi <mayank.mach@hotmail.com>
2022-09-21 19:26:00 +0530

    "Fix Get-vROpsResourceInfo command input to Get-vROpsResource private function" Correcting the auther details "Amended line 25, removed code 'Convertto-JSON'"

--- Public/Get-vROpsResourceInfo.ps1
+++ Public/Get-vROpsResourceInfo.ps1
@@ -22,7 +22,7 @@ function Get-vROpsResourceInfo
     {
         $body = [PSCustomObject]@{
             name = @($Name)
-        } | ConvertTo-Json
+        }
 
         if ($vROpsServer)
         {
